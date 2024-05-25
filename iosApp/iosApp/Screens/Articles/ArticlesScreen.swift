920fe935bb128c90164400e616cd350e8a2aa2e2
otamurod <safarovotamurod@gmail.com>
2024-05-25 23:28:08 +0500

    feat: create iOS Articles Screen

---
+++ iosApp/iosApp/Screens/Articles/ArticlesScreen.swift
@@ -0,0 +1,115 @@
+//
+//  ArticlesScreen.swift
+//  iosApp
+//
+//  Created by Otamurod Safarov on 25/05/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import shared
+import SwiftUI
+
+extension ArticlesScreen {
+    @MainActor
+    class ArticlesViewModelWrapper: ObservableObject {
+        let articlesViewModel: ArticlesViewModel
+
+        init() {
+            articlesViewModel = ArticlesViewModel()
+            articlesState = articlesViewModel.articlesState.value
+        }
+
+        @Published var articlesState: ArticlesState
+
+        func startObserving() {
+            Task {
+                for await articlesS in articlesViewModel.articlesState {
+                    self.articlesState = articlesS
+                }
+            }
+        }
+    }
+}
+
+struct ArticlesScreen: View {
+    @ObservedObject private(set) var viewModel: ArticlesViewModelWrapper
+
+    var body: some View {
+        VStack {
+            AppBar()
+
+            if viewModel.articlesState.loading {
+                Loader()
+            }
+            if let error = viewModel.articlesState.error {
+                ErrorMessage(message: error)
+            }
+            if !viewModel.articlesState.articles.isEmpty {
+                ScrollView {
+                    LazyVStack(spacing: 10) {
+                        ForEach(viewModel.articlesState.articles, id: \.self) { article in
+                            ArticleItemView(article: article)
+                        }
+                    }
+                }
+            }
+        }.onAppear {
+            self.viewModel.startObserving()
+        }
+    }
+}
+
+struct AppBar: View {
+    var body: some View {
+        Text("Articles")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+    }
+}
+
+struct ArticleItemView: View {
+    var article: Article
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            AsyncImage(url: URL(string: article.imageUrl)) { phase in
+                if phase.image != nil {
+                    phase.image!
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                } else if phase.error != nil {
+                    Text("Image Load Error")
+                } else {
+                    ProgressView()
+                }
+            }
+            Text(article.title)
+                .font(.title)
+                .fontWeight(.bold)
+            Text(article.description_)
+            Text(article.date).frame(maxWidth: .infinity, alignment: .trailing).foregroundStyle(.gray)
+        }
+        .padding(16)
+    }
+}
+
+struct Loader: View {
+    var body: some View {
+        ProgressView().frame(maxWidth: .infinity, alignment: .center)
+    }
+}
+
+struct ErrorMessage: View {
+    var message: String
+
+    var body: some View {
+        Text(message)
+            .font(.title)
+    }
+}
+
+struct ArticlesScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticlesScreen(viewModel: .init())
+    }
+}
