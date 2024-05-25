f603d620e69efe332adb1a9b86225bb42b414f74
otamurod <safarovotamurod@gmail.com>
2024-05-25 17:14:12 +0500

    feat: create shared BaseViewModel

---
+++ shared/src/iosMain/kotlin/uz/otamurod/kmp/newsapp/BaseViewModel.kt
@@ -0,0 +1,15 @@
+package uz.otamurod.kmp.newsapp
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.cancel
+
+actual open class BaseViewModel {
+    actual val scope: CoroutineScope
+        get() = CoroutineScope(Dispatchers.IO)
+
+    fun clear(){
+        scope.cancel()
+    }
+}
